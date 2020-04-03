5bc7e7ecc929abb4915ae290024d40409edfa8b6
René van Dorp <dorpr@wigo4it.nl>
2020-04-03 12:25:53 +0200

    Add project files.

---
+++ MyConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
